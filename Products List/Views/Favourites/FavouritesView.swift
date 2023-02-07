f422b1fff23b8b21eb0cb5467abf0156b61f23b5
YK <yacob.kazal@qantas.com.au>
2023-02-07 11:44:37 +1100

    implemented FavouritesView

--- Products List/Views/Favourites/FavouritesView.swift
+++ Products List/Views/Favourites/FavouritesView.swift
@@ -7,7 +7,36 @@
 import SwiftUI
 
 struct FavouritesView: View{
+    @FetchRequest(sortDescriptors: []) var productObjects: FetchedResults<ProductObject>
+    @ObservedObject var productState = ProductState()
+    @Environment(\.managedObjectContext) var moc
     var body: some View {
-        Text("FavouritesView")
+        let favourites = productObjects.filter{$0.isFavourite}
+        if favourites.count > 0 {
+            
+            List(favourites, id: \.self) { product in
+                Text("\(product.title ?? "")")
+                    .onTapGesture {
+                        productState.product = product
+                        productState.showProduct = true
+                    }
+            }
+            .sheet(isPresented: $productState.showProduct) {
+                ProductDetailsView(product: productState.product)
+            }
+        }else{
+            Text("You can add products to your favourites list from product list page")
+                .multilineTextAlignment(.center)
+        }
+    }
+    
+    func loaderView() -> some View {
+        ZStack {
+            Color.black.opacity(0.05)
+                .ignoresSafeArea()
+            ProgressView()
+                .scaleEffect(1, anchor: .center)
+                .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+        }
     }
 }
